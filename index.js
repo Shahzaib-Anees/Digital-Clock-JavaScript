6b6ef76677163ada0a3b99757290ede11f60a110
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-04-24 09:23:58 +0500

    code for clock has been added

---
+++ index.js
@@ -0,0 +1,20 @@
+function showTime(){
+    let curentDate = new Date();
+    let hours = curentDate.getHours();
+    let minutes = (curentDate.getMinutes() + 1);
+    let seconds = (curentDate.getSeconds() + 1);
+    var newDate =new Date().toDateString();
+    console.log(newDate) ;
+    console.log(hours + ":" + minutes + ":" + seconds);
+    let currentTime = hours + ":" + minutes + ":" + seconds;
+    let clock_container =document.getElementsByTagName("span");
+    setTimeout(showTime,1000);
+    clock_container[0].innerText = currentTime ;
+
+    var dateContainer = document.getElementsByTagName("p");
+    dateContainer[0].innerText= newDate;
+}
+
+
+
+
